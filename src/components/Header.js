f0fba0230dea386183bd139062a06d2b35eff785
Vinu Ihalagamage <vinu.i2017@gmail.com>
2022-05-05 11:26:03 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+  return (
+    <header>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+// CSS in JS => add <h1 style={headingStyle} > in header
+
+// const headingStyle = {
+//     color: 'green',
+//     backgroundColor: 'black'
+// }
+
+export default Header
