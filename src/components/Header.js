c033def4d5b8e74ee5bd3684af9e30d0aea350b7
vinui409 <vinu.i2017@gmail.com>
2022-05-05 14:54:58 -0700

    git testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({title}) => {
   return (
     <header>
         <h1>{title}</h1>
-        <Button color='green' text='Add' />
+        <Button color='green' text='Testing Git' />
     </header>
   )
 }
